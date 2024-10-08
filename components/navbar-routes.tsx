f88843e283625446942a05b0e27bab7ae9171f79
mcdukemannguyen <mcdukemanwuthering1@gmail.com>
2024-10-08 08:23:06 +0700

    feat: complete dashboard layout

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const NavbarRoutes = () => {
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      <button className="flex items-center gap-x-2 text-sm font-medium text-slate-500 hover:text-slate-700 transition">
+        {"test"}
+      </button>
+    </div>
+  );
+};
