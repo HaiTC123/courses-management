1a9cb2df79443c130b6f45bd31847914d15e9950
quangle1654 <quangle1654@gmail.com>
2024-11-17 10:35:42 +0700

    fix: correct background color navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center h-full p-4 bg-white border-b shadow-sm ">
+    <div className="flex items-center h-full p-4 bg-white border-b shadow-sm dark:bg-[#020817]">
       <MobileSidebar />
       <NavbarRoutes />
     </div>
