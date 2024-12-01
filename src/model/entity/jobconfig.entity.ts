7e6a78570640a09c84c727b9f23fdb54b59cd8eb
HaiTC123 <hai1999203@gmail.com>
2024-12-01 12:28:13 +0700

    check code

---
+++ src/model/entity/jobconfig.entity.ts
@@ -0,0 +1,41 @@
+import { AutoMap } from '@automapper/classes';
+import { BaseEntity } from './base.entity';
+
+export class JobConfigEntity extends BaseEntity {
+  @AutoMap()
+  id: number;
+
+  @AutoMap()
+  status: string;
+
+  @AutoMap()
+  startTime?: Date;
+
+  @AutoMap()
+  endTime?: Date;
+
+  @AutoMap()
+  cronJob?: string;
+
+  @AutoMap()
+  typeJob: string;
+
+  @AutoMap()
+  rawData?: string;
+
+  @AutoMap()
+  key: string;
+
+  @AutoMap()
+  typeBusiness: string;
+
+  @AutoMap()
+  relatedId?: number;
+
+  @AutoMap()
+  relatedType?: string;
+
+  @AutoMap()
+  jobDetail: string;
+
+}
