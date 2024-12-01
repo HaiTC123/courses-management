7e5645f5505878254d44956a9a5bbb4fad2c9c23
HaiTC123 <hai1999203@gmail.com>
2024-12-01 12:25:24 +0700

    them cau hinh tu dong gui thong bao

---
+++ src/model/dto/jobconfig.dto.ts
@@ -0,0 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { AutoMap } from '@automapper/classes';
+import { BaseDto } from './base.dto';
+
+export class JobConfigDto extends BaseDto {
+    @ApiProperty()
+    @AutoMap()
+    id: number;
+
+    @ApiProperty()
+    @AutoMap()
+    status: string;
+
+    @ApiProperty()
+    @AutoMap()
+    startTime?: Date;
+
+    @ApiProperty()
+    @AutoMap()
+    endTime?: Date;
+
+    @ApiProperty()
+    @AutoMap()
+    cronJob?: string;
+
+    @ApiProperty()
+    @AutoMap()
+    typeJob: string;
+
+    @ApiProperty()
+    @AutoMap()
+    rawData?: string;
+
+    @ApiProperty()
+    @AutoMap()
+    key: string;
+
+    @ApiProperty()
+    @AutoMap()
+    typeBusiness: string;
+
+    @ApiProperty()
+    @AutoMap()
+    relatedId?: number;
+
+    @ApiProperty()
+    @AutoMap()
+    relatedType?: string;
+
+    @AutoMap()
+    jobDetail: string;
+
+}
